29052572a6a34100618b233dd912a05068c0f529
Adithyakr <adithyakr42@gmail.com>
2025-08-14 20:55:58 +0530

    Added backend validators

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import router from '../routes/auth.routes.js';
+import authRoutes from '../routes/auth.routes.js';
 import dbConnect from "../db/index.js"
 
 const app=express();
 dbConnect()
-app.use("/api/v1/auth",router)
+app.use("/api/v1/auth",authRoutes)
 
 export default app;
